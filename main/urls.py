7cf10a67670c12c0744c2cf69d95ab3f0bedc701
nelsonpenha <nelsonpenha10@gmail.com>
2025-04-04 07:10:27 -0400

    Iniciar sesion

--- main/urls.py
+++ main/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from producto import views
 
 urlpatterns = [
-    path('', views.lista_productos, name='lista_productos'),
+    path('admin/', admin.site.urls),
+    path('producto/', views.lista_productos, name='lista_productos'),
     path('crear/', views.crear_producto, name='crear_producto'),
     path('actualizar/<int:pk>/', views.actualizar_producto, name='actualizar_producto'),
     path('eliminar/<int:pk>/', views.eliminar_producto, name='eliminar_producto'),
+    path('usuario/crear/', views.crear_usuario, name='crear_usuario'),
+    path('', views.iniciar_sesion, name='iniciar_sesion'),
 ]
--- producto/views.py
+++ producto/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.db import IntegrityError
+
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from producto.forms import ProductoForm
 from producto.models import Producto
+
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -37,3 +43,24 @@ def eliminar_producto(request, pk):
         producto.delete()
         return redirect('lista_productos')
     return render(request, 'eliminar_producto.html', {'producto': producto})
+
+def crear_usuario(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(username=request.POST['username'],  password =request.POST['password1'])
+                login(request, user)
+                return redirect('lista_productos')
+            except IntegrityError:
+                return render(request, 'crear_usuario.html', {"form": UserCreationForm, "error": "El usuario ya existe"})
+        return render(request, 'crear_usuario.html', {'form': UserCreationForm, "error": "Las contraseñas no coinciden"})
+    return render(request, 'crear_usuario.html', {'form': UserCreationForm})
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return redirect('lista_productos')
+        return render(request, 'iniciar_sesion.html', {"form": AuthenticationForm, "error": "Usuario o contraseña incorrecto"})
+    return render(request, 'iniciar_sesion.html', {"form": AuthenticationForm})
